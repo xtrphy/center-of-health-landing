efb0392eace77a8d65ff394db9b58ce097a34bd1
xtrphy <perepeckinvlad@gmail.com>
2025-01-25 21:08:41 +0500

    Add new about section

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 const proceduresBtn = document.querySelector('.hero-action-button');
-const signBtn = document.querySelector('.action-button');
 
 proceduresBtn.addEventListener('click', () => {
   proceduresBtn.innerText = 'Загрузка...';
   setInterval(() => {
-    button.innerText = 'Процедуры лечения';
-  }, 2000);
-});
-
-signBtn.addEventListener('click', () => {
-  signBtn.innerText = 'Спасибо!';
-  setInterval(() => {
-    signBtn.innerText = 'Запись на прием';
+    proceduresBtn.innerText = 'Процедуры лечения';
   }, 2000);
 });
